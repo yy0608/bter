6639d8f2269d3b9dd0dcb4ae27c8c7199aa28d70
gaoyouyi <447462224@qq.com>
2022-10-26 14:02:42 +0800

    加入getBytes函数

--- bter.js
+++ bter.js
@@ -1,3 +1,34 @@
+// 实现Java的getBytes
+const strToUtf8Bytes = str => {
+  const utf8 = [];
+  for (let ii = 0; ii < str.length; ii++) {
+    let charCode = str.charCodeAt(ii);
+    if (charCode < 0x80) utf8.push(charCode);
+    else if (charCode < 0x800) {
+      utf8.push(0xc0 | (charCode >> 6), 0x80 | (charCode & 0x3f));
+    } else if (charCode < 0xd800 || charCode >= 0xe000) {
+      utf8.push(0xe0 | (charCode >> 12), 0x80 | ((charCode >> 6) & 0x3f), 0x80 | (charCode & 0x3f));
+    } else {
+      ii++;
+      charCode = 0x10000 + (((charCode & 0x3ff) << 10) | (str.charCodeAt(ii) & 0x3ff));
+      utf8.push(
+        0xf0 | (charCode >> 18),
+        0x80 | ((charCode >> 12) & 0x3f),
+        0x80 | ((charCode >> 6) & 0x3f),
+        0x80 | (charCode & 0x3f),
+      );
+    }
+  }
+  // 兼容汉字，ASCII码表最大的值为127，大于127的值为特殊字符
+  for(let jj=0;jj<utf8.length;jj++){
+	  var code = utf8[jj];
+	  if(code>127){
+		  utf8[jj] = code - 256;
+	  }
+  }
+  return utf8;
+}
+
 class BTER {
   constructor () {
     if (!BTER.instance) {
@@ -57,11 +88,19 @@ class BTER {
     return !this.bluetoothSocket.isConnected()
   }
 
-  // 指令打印，如果缓慢，可自己实现
+  // 指令打印，默认使用安卓的getBytes，但在uniapp中要7s，需要自己实现
   printSomething (zplCode) {
     var outputStream = this.bluetoothSocket.getOutputStream();
     plus.android.importClass(outputStream);
-    var arrayBuffer = plus.android.invoke(zplCode,'getBytes','gbk')
+    let arrayBuffer
+    try {
+      arrayBuffer = strToUtf8Bytes(zplCode)
+      if (arrayBuffer == '' || (Object.prototype.toString.call(arrayBuffer) === '[object Array]' && arrayBuffer.length === 0)) {
+        arrayBuffer = plus.android.invoke(zplCode,'getBytes','gbk')
+      }
+    } catch {
+      arrayBuffer = plus.android.invoke(zplCode,'getBytes','gbk')
+    }
     outputStream.write(arrayBuffer);
     outputStream.flush();
   }
